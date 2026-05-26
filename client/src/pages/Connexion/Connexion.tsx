9960ca736b86047369a726e2a4ddac021ad5e57b
nellystephane <115810420+nellystephane@users.noreply.github.com>
2026-05-26 07:21:39 +0100

    update client/src/pages/Connexion/Connexion.tsx

--- client/src/pages/Connexion/Connexion.tsx
+++ client/src/pages/Connexion/Connexion.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { ArrowLeft, Mail, Lock, LogIn } from 'lucide-react';
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../context/AuthContext.tsx';
 
 interface FormErrors {
   email?: string;
