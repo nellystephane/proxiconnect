6694e8ff982e8d04b2203b17dba15e4e51cbc69d
nellystephane <115810420+nellystephane@users.noreply.github.com>
2026-05-26 08:16:04 +0100

    update client/src/App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -36,12 +36,10 @@ function AppContent() {
               <div>Profil (à créer)</div>
             </ProtectedRoute>
           } />
-         // dans les routes :
-<Route path="/deposer" element={
-  <ProtectedRoute>
-    <Deposer />
-  </ProtectedRoute>
-} />
+          <Route path="/deposer" element={
+            <ProtectedRoute>
+              <Deposer/>
+            </ProtectedRoute>
           } />
           {/* autres routes protégées */}
         </Routes>
