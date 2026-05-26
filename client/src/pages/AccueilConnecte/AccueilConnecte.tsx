c380e1f82ec35e8d91ee623264277b199d296ed7
nellystephane <115810420+nellystephane@users.noreply.github.com>
2026-05-26 08:21:16 +0100

    update client/src/pages/AccueilConnecte/AccueilConnecte.tsx

--- client/src/pages/AccueilConnecte/AccueilConnecte.tsx
+++ client/src/pages/AccueilConnecte/AccueilConnecte.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../context/AuthContext.tsx';
 import AnnonceCard from '../../components/AnnonceCard/AnnonceCard';
 import API from '../../api/axios';
 
