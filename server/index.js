e2a35241db747ad3dc93d4f2d19747541cb12efd
nellystephane <115810420+nellystephane@users.noreply.github.com>
2026-05-28 08:16:17 +0100

    update server/index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
-
+const uploadRoutes = require('./routes/uploadRoutes');
 const connectDB = require('./config/db');
 
 
